afe5cc7f9f2afcd4684677ffdae13fa72d2ebb7d
joseph wainaina <111576688+josephynaina@users.noreply.github.com>
2023-09-01 00:58:11 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-Island Perimeter
-"""
 
 def island_perimeter(grid):
     """
@@ -9,7 +6,7 @@ def island_perimeter(grid):
     :param grid:
     :return:
     """
-   def island_perimeter(grid):
+
     if len(grid) == 0 or len(grid[0]) == 0:
         return 0
 
@@ -18,13 +15,13 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
-                if j == 0 or grid[i][j - 1] == 0: 
+                if j == 0 or grid[i][j - 1] == 0:
                     count += 1
                 if j == (len(grid[0]) - 1) or grid[i][j + 1] == 0:
                     count += 1
                 if i == (len(grid) - 1) or grid[i + 1][j] == 0:
                     count += 1
-                if grid[i -1][j] == 0 or i == 0:
+                if grid[i - 1][j] == 0 or i == 0:
                     count += 1
 
     return count
